9c61196cbf04fcb03889d9d1ca0b4ad77d099ab4
emmalinnearoos@gmail.com <emmalinnearoos@gmail.com>
2020-12-04 09:04:42 +0100

    First commit, created methods for user input and Main Meny

--- MembershipInlamningsUppgift/Program.cs
+++ MembershipInlamningsUppgift/Program.cs
@@ -6,7 +6,45 @@ namespace MembershipInlamningsUppgift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter secret word: ");
+            string pass = Console.ReadLine().ToLower();
+            //CheckPass kontrollerar användarens svar
+            CheckPass(pass);
+
+
+            Console.ReadKey();
+        }
+        static void CheckPass(string pass)
+        {
+            int numberOfTries = 3;
+
+            while(numberOfTries >= 0)
+            {
+                if (pass == "götebuggarna")
+                {
+                    Console.WriteLine("Correct!");
+                    //MainMeny tar dig vidare till huvudmenyn
+                    MainMeny();
+                    break;
+                }
+                else
+                {
+                    numberOfTries--;
+                    Console.WriteLine($"Incorrect. You have {numberOfTries} tries left: ");
+                    pass = Console.ReadLine().ToLower();
+                }
+            }
+        }
+        static void MainMeny()
+        {
+            string[] mainMeny = new string[] { "Meet the members", "Join the club!", "Remove a member", "Log out" };
+
+            Console.WriteLine("This is our meny: ");
+
+            for(int i = 0; i < mainMeny.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}.{mainMeny[i]}");
+            }
         }
     }
 }
